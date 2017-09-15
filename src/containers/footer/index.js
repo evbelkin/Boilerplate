9651dca8e2f6946db37d57660f91fa6ca70daae9
Evgeny Belkin <sunafajro@gmail.com>
2017-09-15 23:25:35 +0300

    добавлен футер

---
+++ src/containers/footer/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Footer extends React.Component {
+  render() {
+    return (
+      <footer className="footer">
+        <div className="container-fluid">
+          <span className="text-muted">Place sticky footer content here.</span>
+        </div>
+      </footer>
+    );
+  }
+}
+
+export default Footer;
