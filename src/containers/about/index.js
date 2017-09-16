2f53e8b2d70705750d55213e60ad2e626bc1b462
Evgeny Belkin <sunafajro@gmail.com>
2017-09-16 15:29:05 +0300

    меняем обычные ссылки на объекты роутера

--- src/containers/about/index.js
+++ src/containers/about/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 class About extends React.Component {
   render() {
     return (
       <div>
         <ol className="breadcrumb">
-          <li className="breadcrumb-item"><a href="#">Home</a></li>
+          <li className="breadcrumb-item"><Link to='/'>Home</Link></li>
           <li className="breadcrumb-item active">About</li>
         </ol>
         <h3>About page</h3>
--- src/containers/login/index.js
+++ src/containers/login/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
+import { Link } from 'react-router-dom'
 
 class Login extends React.Component {
   render() {
     return (
       <div>
         <ol className="breadcrumb">
-          <li className="breadcrumb-item"><a href="#">Home</a></li>
+          <li className="breadcrumb-item"><Link to='/'>Home</Link></li>
           <li className="breadcrumb-item active">Login</li>
         </ol>
         <h3>Login page</h3>
