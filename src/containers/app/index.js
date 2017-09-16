90f7d97e5adf093dff360f6717b58a156343b9e5
Evgeny Belkin <sunafajro@gmail.com>
2017-09-16 16:47:53 +0300

    переоформляем код и исправляем импорт компонентов

--- src/containers/app/index.js
+++ src/containers/app/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import Navigation from '../navigation/index';
 import Home from '../home/index';
-import About from '../about/index';
+import { About } from '../about/index';
 import Login from '../login/index';
-import Footer from '../footer/index';
+import { Footer } from '../footer/index';
 
 class App extends React.Component {
   render() {
--- src/containers/footer/index.js
+++ src/containers/footer/index.js
@@ -1,15 +1,9 @@
 import React from 'react';
 
-class Footer extends React.Component {
-  render() {
-    return (
-      <footer className="footer">
-        <div className="container">
-          <span className="text-muted">Place sticky footer content here.</span>
-        </div>
-      </footer>
-    );
-  }
-}
-
-export default Footer;
+export const Footer = () => (
+  <footer className="footer">
+    <div className="container">
+      <span className="text-muted">Place sticky footer content here.</span>
+    </div>
+  </footer>
+);
