70ecfd65d6cb98ac438447ab8b6d3afac49e48ad
Evgeny Belkin <sunafajro@gmail.com>
2017-09-16 17:32:03 +0300

    элементы навигации получаем из store

---
+++ src/modules/actions/navigation.js
@@ -0,0 +1,23 @@
+export const GET_NAVIGATION_REQUESTED = 'GET_NAVIGATION_REQUESTED';
+export const GET_NAVIGATION = 'GET_NAVIGATION';
+
+export const getNavigationLinks = () => {
+  return dispatch => {
+    dispatch({
+      type: GET_NAVIGATION_REQUESTED
+    });
+
+    return setTimeout(() => {
+      dispatch({
+        type: GET_NAVIGATION,
+        links: LINKS
+      })
+    }, 2000);
+  }
+};
+
+const LINKS = [
+  { id: 'home', title: 'Home', value: '/'},
+  { id: 'about', title: 'About', value: '/about'},
+  { id: 'login', title: 'Login', value: '/login'}
+];
--- src/modules/index.js
+++ src/modules/index.js
@@ -1,8 +1,10 @@
-import { combineReducers } from 'redux'
-import { routerReducer } from 'react-router-redux'
+import { combineReducers } from 'redux';
+import { routerReducer } from 'react-router-redux';
+import navigation from './reducers/navigation';
 import home from './reducers/home';
 
 export default combineReducers({
   routing: routerReducer,
+  navigation,
   home
 })
