034094567e2360d0bfcc79816f6e68948c2fb749
Evgeny Belkin <sunafajro@gmail.com>
2017-09-16 16:40:26 +0300

    добавлен текст к страничке О сайте

--- src/containers/about/index.js
+++ src/containers/about/index.js
@@ -10,6 +10,7 @@ class About extends React.Component {
           <li className="breadcrumb-item active">About</li>
         </ol>
         <h3>About page</h3>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit quos necessitatibus sequi facere esse ipsum eligendi porro ut, dolores reiciendis quibusdam voluptas error! Quis, possimus facilis. Maiores doloribus a dicta.</p>
       </div>
     );
   }
